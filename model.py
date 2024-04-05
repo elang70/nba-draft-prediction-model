19be8e097025cf6cd6034d7cd1396d820b8d8002
Yahya Syed <yisyed98@gmail.com>
2024-04-05 14:43:20 -0500

    added sample logistic regression

--- model.py
+++ model.py
@@ -1,1 +1,24 @@
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+# read data
+df = pd.read_csv('nba_draft_combine_all_years.csv')
+# drop useless columns
+df.drop(['Player', 'Year', 'Unnamed: 0'], axis=1, inplace=True)
+# make draft pick column binary
+df['Draft pick'] = df['Draft pick'].apply(lambda x: 0 if pd.isna(x) or x == 0 else 1)
+
+# split data into features and target
+X = df.drop('Draft pick', axis=1)
+y = df['Draft pick']
+X.dropna(inplace=True)
+y = y[X.index]
+
+# split data into training and testing
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# train model
+clf = LogisticRegression(random_state=0).fit(x_train, y_train)
+# score on test data
+print(clf.score(x_test, y_test))
